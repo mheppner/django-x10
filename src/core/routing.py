86201eae1db72d3a33332bb73d6dfc761ae4cba5
Mark Heppner <mheppner@users.noreply.github.com>
2017-11-21 14:41:28 -0500

    Change websocket path

--- src/core/routing.py
+++ src/core/routing.py
@@ -4,10 +4,10 @@ from channels.routing import route
 from . import consumers
 
 
-UNITS_PATH = r'^/units/status/'
+PATH = r'^/status/'
 
 routes = [
-    route('websocket.connect', consumers.ws_connect, path=UNITS_PATH),
-    route('websocket.receive', consumers.ws_receive, path=UNITS_PATH),
-    route('websocket.disconnect', consumers.ws_disconnect, path=UNITS_PATH),
+    route('websocket.connect', consumers.ws_connect, path=PATH),
+    route('websocket.receive', consumers.ws_receive, path=PATH),
+    route('websocket.disconnect', consumers.ws_disconnect, path=PATH),
 ]
